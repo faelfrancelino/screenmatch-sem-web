a238b0177daf4863ce535cfeed6dab1dd321541f
faelfrancelino <faelfrancelino@gmail.com>
2024-07-24 15:58:14 -0300

    curso alura

--- sreenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ sreenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,43 @@
 package br.com.alura.screenmatch.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
-	@GetMapping("/series")
-	public String obterSeries() {
-		return "Hello World!";
+	@Autowired
+	private SerieService serieService;
+
+	@GetMapping
+	public List<SerieDTO> obterSeries() {
+		return serieService.obterTodasAsSeries();
+	}
+	
+	@GetMapping("/top5")
+	public List<SerieDTO> obterTop5Series() {
+		return serieService.obterTop5Series();
+	}
+
+	@GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamentos() {
+        return serieService.obterLancamentos();
+    }
+	
+	@GetMapping("/{id}")
+	public SerieDTO obterPorId(@PathVariable Long id) {
+		return serieService.obterPorId(id);
 	}
 	
 }

9d9716e73e5664dc4db1f48865f4c64405e6f333
faelfrancelino <faelfrancelino@gmail.com>
2024-07-24 16:33:46 -0300

    final screen match

--- sreenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ sreenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,4 +41,18 @@ public class SerieController {
 		return serieService.obterPorId(id);
 	}
 	
+	@GetMapping("/{id}/temporadas/todas")
+	public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return serieService.obterTodasTemporadas(id);
+	}
+	
+	@GetMapping("/{id}/temporadas/{numero}")
+	public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numero){
+	        return serieService.obterTemporadasPorNumero(id, numero);
+	}
+	
+	@GetMapping("/categoria/{nomeGenero}")
+	public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero){
+	        return serieService.obterSeriesPorCategoria(nomeGenero);
+	}
 }
--- sreenmatch/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ sreenmatch/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
@@ -32,7 +34,7 @@ public class SerieService {
     }
 
 	public List<SerieDTO> obterLancamentos() {
-		return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+		return converteDados(repositorio.lancamentosMaisRecentes());
 	}
 
 	public SerieDTO obterPorId(Long id) {
@@ -43,5 +45,28 @@ public class SerieService {
 		}
 		return null;
 	}
+	
+	public List<EpisodioDTO> obterTodasTemporadas(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
 
+        if (serie.isPresent()) {
+                Serie s = serie.get();
+                return s.getEpisodios().stream()
+                                .map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
+                                .collect(Collectors.toList());
+        }
+        return null;	
+	}
+
+	public List<EpisodioDTO> obterTemporadasPorNumero(Long id, Long numero) {
+        return repositorio.obterEpisodiosPorTemporada(id, numero)
+                        .stream()
+                        .map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
+                        .collect(Collectors.toList());
+	}
+	
+	public List<SerieDTO> obterSeriesPorCategoria(String nomeGenero) {
+		Categoria categoria = Categoria.fromPortugues(nomeGenero);
+		return converteDados(repositorio.findByGenero(categoria));
+	}
 }
